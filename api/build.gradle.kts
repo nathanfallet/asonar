98af4ef50fdb85d1c4576953efc7c870fe7518c9
NathanFallet <contact@nathanfallet.me>
2026-08-27 16:09:59 +0200

    Add Maven publishing configuration and Apache 2.0 license

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,34 @@ plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.serialization)
     alias(libs.plugins.kover)
+    alias(libs.plugins.maven)
+}
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    pom {
+        name.set("asonar-api")
+        description.set("Models and API contract of asonar, shared with the clients.")
+        url.set(project.ext.get("url")?.toString())
+        licenses {
+            license {
+                name.set(project.ext.get("license.name")?.toString())
+                url.set(project.ext.get("license.url")?.toString())
+            }
+        }
+        developers {
+            developer {
+                id.set(project.ext.get("developer.id")?.toString())
+                name.set(project.ext.get("developer.name")?.toString())
+                email.set(project.ext.get("developer.email")?.toString())
+                url.set(project.ext.get("developer.url")?.toString())
+            }
+        }
+        scm {
+            url.set(project.ext.get("scm.url")?.toString())
+        }
+    }
 }
 
 kotlin {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
 plugins {
     alias(libs.plugins.jvm) apply false
     alias(libs.plugins.multiplatform) apply false
+    alias(libs.plugins.maven) apply false
     alias(libs.plugins.kover)
 }
 
 allprojects {
     group = "me.nathanfallet.asonar"
     version = "0.1.0"
+    project.ext.set("url", "https://github.com/nathanfallet/asonar")
+    project.ext.set("license.name", "Apache 2.0")
+    project.ext.set("license.url", "https://www.apache.org/licenses/LICENSE-2.0.txt")
+    project.ext.set("developer.id", "nathanfallet")
+    project.ext.set("developer.name", "Nathan Fallet")
+    project.ext.set("developer.email", "contact@nathanfallet.me")
+    project.ext.set("developer.url", "https://www.nathanfallet.me")
+    project.ext.set("scm.url", "https://github.com/nathanfallet/asonar.git")
 
     repositories {
         mavenCentral()
